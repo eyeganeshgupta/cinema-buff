c743c289664d5dd691f053e556cbe1537d253cd5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-01-05 18:48:53 +0530

    Component: MovieCard

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -14,6 +14,28 @@ const MovieCard = ({ data, fromSearch, mediaType }) => {
   const posterUrl = data.poster_path
     ? url?.poster + data.poster_path
     : PosterFallback;
+  return (
+    <div
+      className="movie-card"
+      onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
+    >
+      <div className="poster-block">
+        <Img className="poster-img" src={posterUrl} />
+        {!fromSearch && (
+          <React.Fragment>
+            <CircleRating rating={data.vote_average.toFixed(1)} />
+            <Genres data={data.genre_ids.slice(0, 2)} />
+          </React.Fragment>
+        )}
+      </div>
+      <div className="text-block">
+        <span className="title">{data.title || data.name}</span>
+        <span className="date">
+          {dayjs(data.release_date).format("MMM D, YYYY")}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
