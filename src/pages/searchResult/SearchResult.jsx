2fdadabbd7ac582271eb1f0c33d63134c2d7a85d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-01-19 09:16:03 +0530

    Component: SearchResult

---
+++ src/pages/searchResult/SearchResult.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./searchresult.scss";
+import { fetchDataFromAPI } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
+import noResults from "../../assets/no-results.png";
+
+const SearchResult = () => {
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  return (
+    <div className="search-results-page">
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="page-title">
+                {`Search ${
+                  data?.total_results > 1 ? "results" : "result"
+                } of '${query}'`}
+              </div>
+              <InfiniteScroll
+                className="content"
+                dataLength={data?.results?.length || []}
+                next={fetchNextPageData}
+                hasMore={pageNum <= data?.total_pages}
+                loader={<Spinner />}
+              >
+                {data?.results.map((item, index) => {
+                  if (item.media_type === "person") return;
+                  return (
+                    <MovieCard key={index} data={item} fromSearch={true} />
+                  );
+                })}
+              </InfiniteScroll>
+            </>
+          ) : (
+            <span className="result-not-found">Sorry, Results not found!</span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
+  );
+};
+
+export default SearchResult;
